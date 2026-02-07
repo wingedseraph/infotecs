ad4b5dc7943133a1e37419fe1a55ae0f1d01e73c
wingedseraph <r89647@gmail.com>
2026-02-07 12:27:41 +0300

    feat(const): add messages for response

--- src/shared/api/constants.ts
+++ src/shared/api/constants.ts
@@ -1,3 +1,18 @@
 export const AUTH_MESSAGE = {
   INVALID_CREDENTIAL: 'Неверный логин или пароль',
 } as const;
+
+export const USER_RESPONSE_MESSAGE = {
+  SUCCESSFUL_CREATED: 'Пользователь успешно создан',
+  SUCCESSFUL_UPDATED: 'Пользователь успешно обновлен',
+  SUCCESSFUL_DELETED: 'Пользователь успешно удален',
+
+  FAILED_CREATED: 'Ошибка при создании пользователя:',
+  FAILED_UPDATED: 'Ошибка при обновлении пользователя:',
+  FAILED_DELETED: 'Ошибка при удалении пользователя:',
+};
+
+export const GENERIC_RESPONSE_MESSAGE = {
+  WRONG_API_DATA: 'Некорректные данные с API',
+  SOMETHING_WENT_WRONG: 'Что-то пошло не так',
+};
